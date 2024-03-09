bbdd4ad0909461a5c8990c586f9a3fbef0020b16
Austin <austinngobeni2002@gmail.com>
2024-03-09 22:22:09 +0200

    add input scanner

--- Main.java
+++ Main.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
+
 public class Main {
 	public static void main(String[] args) {
+
+		String typeOfProgression = inputReader();
 		Progression prog;
 		prog = new FibonacciProgression(0, 1);
 		prog.printProgression(10);
+	}
+
+	public static String inputReader() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Which progression do you want to see? ");
+		String inputText = input.nextLine();
+		return inputText;
 	}
 }
